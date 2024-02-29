62a3ab12dc74280c55a8c2a51ffb3ea9b0bb27a8
vivinero <viviannzemeke@gmail.com>
2024-02-29 15:13:02 +0100

    updated

--- models/userModel.js
+++ models/userModel.js
@@ -16,9 +16,6 @@ const patientSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    confirmPassword: {
-        type: String,
-    },
     isVerified :{
         type : Boolean,
         default : false
