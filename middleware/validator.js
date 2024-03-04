343d542bb80c83f88180af6d74bff921ec2b434c
vivinero <viviannzemeke@gmail.com>
2024-03-04 13:43:05 +0100

    update

--- middleware/validator.js
+++ middleware/validator.js
@@ -9,16 +9,19 @@ const validateUser = (data) => {
                 'string.empty': "First name field can't be left empty",
                 'string.min': "Minimum of 3 characters for the first name field",
                 'any.required': "Please first name is required",
-                "string.pattern.base": "No special characters allowed on firstname"
+                "string.pattern.base": "No special characters allowed on first name"
             }),
             lastName: joi.string().min(3).max(30).regex(/^[a-zA-Z]+$/).trim().required().messages({
                 'string.empty': "Last name field can't be left empty",
                 'string.min': "Minimum of 3 characters for the last name field",
-                'any.required': "Please last name is required"
+                'any.required': "Please last name is required",
+                "string.pattern.base": "No special characters allowed on last name"
+
             }),
             email: joi.string().max(40).trim().email( {tlds: {allow: false} } ).required().messages({
                 'string.empty': "Email field can't be left empty",
                 'any.required': "Please Email is required"
+                
             }),
 
             password: joi.string()
